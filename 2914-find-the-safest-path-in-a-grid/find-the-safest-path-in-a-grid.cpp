880faf66935d5cfbbfc0993314f27b5995b6663d
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-02 08:32:24 +0530

    Time: 268 ms (94.62%) | Memory: 123.7 MB (93.68%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return 0;
+        vector<vector<int>> safe(n, vector<int>(n, -1));
 
-        vector<vector<int>> dist(n, vector<int>(n, -1));
-        queue<pair<pair<int, int>, int>> q;
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return 0;
+
+        queue<pair<int, int>> q;
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == 1) {
-                    q.push({{i, j}, 0});
-                    dist[i][j] = 0;
+                    q.push({i, j});
+                    safe[i][j] = 0;
                 }
             }
         }
@@ -19,43 +20,44 @@ public:
         int delCol[4] = {0, 1, 0, -1};
 
         while(!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int time = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
 
             for(int i=0; i<4; i++) {
                 int newRow = row + delRow[i];
                 int newCol = col + delCol[i];
-                if(newRow >= 0 && newRow < n && newCol >=0 && newCol < n && dist[newRow][newCol] == -1) {
-                    dist[newRow][newCol] = time + 1;
-                    q.push({{newRow, newCol}, time+1});
+
+                if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < n && safe[newRow][newCol] == -1) {
+                    safe[newRow][newCol] = safe[row][col] + 1;
+                    q.push({newRow, newCol});
                 }
             }
         }
 
         priority_queue<pair<int, pair<int, int>>> pq;
-        vector<vector<int>> vis(n, vector<int>(n, false));
-        pq.push({dist[0][0], {0, 0}});
+        pq.push({safe[0][0], {0, 0}});
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
         vis[0][0] = true;
 
         while(!pq.empty()) {
-            int safenessOfPath = pq.top().first;
+            int safeness = pq.top().first;
             int row = pq.top().second.first;
             int col = pq.top().second.second;
             pq.pop();
 
             if(row == n-1 && col == n-1) {
-                return safenessOfPath;
+                return safeness;
             }
 
             for(int i=0; i<4; i++) {
                 int newRow = row + delRow[i];
                 int newCol = col + delCol[i];
-                if(newRow >= 0 && newRow < n && newCol >=0 && newCol < n && !vis[newRow][newCol]) {
+
+                if(newRow >= 0 && newCol >= 0 && newRow < n && newCol < n && !vis[newRow][newCol]) {
                     vis[newRow][newCol] = true;
-                    int next_safe = min(safenessOfPath, dist[newRow][newCol]);
-                    pq.push({next_safe, {newRow, newCol}});
+                    int newSafe = min(safeness, safe[newRow][newCol]);
+                    pq.push({newSafe, {newRow, newCol}});
                 }
             }
         }
