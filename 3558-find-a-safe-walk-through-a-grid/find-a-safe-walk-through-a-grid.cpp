acabd769567e4c15a04b4987fd35f165b4a6e22d
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-02 08:11:36 +0530

    Time: 4 ms (96.61%) | Memory: 32.7 MB (47.15%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -4,37 +4,47 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        priority_queue<pair<int, pair<int, int>>> pq;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        pq.push({health-grid[0][0], {0, 0}});
-        vis[0][0] = 1;
+        vector<vector<int>> maxHealth(m, vector<int>(n, -1));
+        deque<pair<int, pair<int, int>>> dq;
+
+        int startHealth = health - grid[0][0];
+        if(startHealth < 1) return false;
+
+        dq.push_front({startHealth, {0, 0}});
+        maxHealth[0][0] = startHealth;
 
         int delRow[4] = {-1, 0, 1, 0};
         int delCol[4] = {0, 1, 0, -1};
-
-        while(!pq.empty()) {
-            int currHealth = pq.top().first;
-            int row = pq.top().second.first;
-            int col = pq.top().second.second;
-            pq.pop();
+        
+        while(!dq.empty()) {
+            int currHealth = dq.front().first;
+            int row = dq.front().second.first;
+            int col = dq.front().second.second;
+            dq.pop_front();
 
             if(row == m-1 && col == n-1 && currHealth >= 1) {
                 return true;
-            } else if(currHealth < 1) {
-                return false;
             }
-
+            
             for(int i=0; i<4; i++) {
                 int newRow = row + delRow[i];
                 int newCol = col + delCol[i];
 
-                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !vis[newRow][newCol]) {
-                    vis[newRow][newCol] = 1;
-                    pq.push({currHealth - grid[newRow][newCol], {newRow, newCol}});
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+                    int nextHealth = currHealth - grid[newRow][newCol];
+
+                    if(nextHealth >= 1 && nextHealth > maxHealth[newRow][newCol]) {
+                        maxHealth[newRow][newCol] = nextHealth;
+                        if(grid[newRow][newCol] == 0) {
+                            dq.push_front({nextHealth, {newRow, newCol}});
+                        } else {
+                            dq.push_back({nextHealth, {newRow, newCol}});
+                        }
+                    }
                 }
             }
         }
 
-        return true;
+        return false;
     }
 };
