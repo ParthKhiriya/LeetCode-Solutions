9d1a264d656f0b448f4285e9fbe9976de68d2ce5
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-14 21:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (13.34%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+
+        // Creating a queue to store the nodes as we traverse. 
+        queue<TreeNode*> q;
+        q.push(root);
+        int height = 0;
+
+        while(!q.empty()) {
+            int levelSize = q.size();
+
+            for(int i=0; i<levelSize; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            
+            height++;
+        }
+
+        return height;
+    }
+};
