1f21222431ecdaa8e9cd186f4b2bb44b4bb52dcf
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-15 18:36:34 +0530

    Time: 375 ms (76.52%) | Memory: 292.1 MB (41.74%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     int subsequencePairCount(vector<int>& nums) {
         int MOD = 1e9 + 7;
+        int n = nums.size();
         int max_val = *max_element(nums.begin(), nums.end());
 
-        // Now create a dp array to store every possible number of ways for every possible g1 and g2.
         vector<vector<int>> dp(max_val+1, vector<int>(max_val+1, 0));
         dp[0][0] = 1;
 
         for(int num: nums) {
             vector<vector<int>> next_dp(max_val+1, vector<int>(max_val+1, 0));
 
-            for(int g1 = 0; g1 <= max_val; g1++) {
-                for(int g2 = 0; g2 <= max_val; g2++) {
+            for(int g1=0; g1<=max_val; g1++) {
+                for(int g2=0; g2<=max_val; g2++) {
                     if(dp[g1][g2] == 0) continue;
                     int ways = dp[g1][g2];
 
-                    // 1. Either skip the current element
-                    next_dp[g1][g2] = (next_dp[g1][g2] + ways)  % MOD;
+                    next_dp[g1][g2] = (next_dp[g1][g2] + ways) % MOD;
 
-                    // 2. Add current element to subsequence 1
                     int next_g1 = gcd(g1, num);
                     next_dp[next_g1][g2] = (next_dp[next_g1][g2] + ways) % MOD;
 
-                    // 3. Add current element to subsequence 2
                     int next_g2 = gcd(g2, num);
                     next_dp[g1][next_g2] = (next_dp[g1][next_g2] + ways) % MOD;
                 }
             }
-
             dp = move(next_dp);
         }
 
