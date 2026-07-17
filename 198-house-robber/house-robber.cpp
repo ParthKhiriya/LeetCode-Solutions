2b40feadf670ac582b26e076afd46f23209822e3
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-17 18:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.63%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,14 +5,15 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int one = nums[0];
+        int two = max(nums[0], nums[1]);
 
         for(int i=2; i<n; i++) {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+            int result = max(two, nums[i] + one);
+            one = two;
+            two = result;
         }
 
-        return dp[n-1];
+        return two;
     }
 };
