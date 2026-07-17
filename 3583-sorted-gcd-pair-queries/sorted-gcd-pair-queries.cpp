64b9604d581c26ca496587bc545b7ce80caaf9c5
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-17 12:35:04 +0530

    Time: 39 ms (78.67%) | Memory: 129.7 MB (66.67%) - LeetSync

---
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int max_val = 0;
+        for(int num: nums) {
+            max_val = max(max_val, num);
+        }
+
+        vector<long long> count(max_val + 1, 0);
+        for(int num: nums) {
+            count[num]++;
+        }
+
+        vector<long long> exact_gcd(max_val + 1, 0);
+        for(int i=max_val; i>=1; i--) {
+            long long mult_count = 0;
+
+            for(int j=i; j<=max_val; j+=i) {
+                mult_count += count[j];
+            }
+
+            long long pairs = mult_count * (mult_count - 1)/2;
+
+            for(int j=2*i; j<=max_val; j+=i) {
+                pairs -= exact_gcd[j];
+            }
+
+            exact_gcd[i] = pairs;
+        }
+
+        vector<long long> prefix(max_val + 1, 0);
+        for(int i=1; i<=max_val; i++) {
+            prefix[i] = prefix[i-1] + exact_gcd[i];
+        }
+
+        vector<int> ans;
+        ans.reserve(queries.size());
+        for(long long q: queries) {
+            int index = upper_bound(prefix.begin(), prefix.end(), q) - prefix.begin();
+            ans.push_back(index);
+        }
+
+        return ans;
+    }
+};
