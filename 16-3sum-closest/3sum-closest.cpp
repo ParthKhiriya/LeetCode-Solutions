ba9d2ea8e55eca851ecb99d899f4bc76560dafab
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-18 08:38:38 +0530

    Time: 9 ms (69.10%) | Memory: 14 MB (80.24%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
-
-        // Sort the array for directional certainity
         sort(nums.begin(), nums.end());
-        int result = nums[0] + nums[1] + nums[2];
+
+        int ans = nums[0] + nums[1] + nums[2];
 
         for(int i=0; i<n-2; i++) {
             int left = i+1;
@@ -13,16 +12,19 @@ public:
 
             while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(abs(target - sum) < abs(target - result)) {
-                    result = sum;
-                }
 
-                if(sum == target) return target;
-                else if(sum < target) left++;
-                else right--;
+                if(abs(target - sum) < abs(target - ans)) {
+                    ans = sum;
+                }
+                if(sum ==  target) {
+                    return target;
+                } else if(sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
         }
-
-        return result;
+        return ans;
     }
 };
