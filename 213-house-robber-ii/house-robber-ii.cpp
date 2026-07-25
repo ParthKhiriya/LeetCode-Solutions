0db9a0b5c3bc52412f8a9952110ccb75fe39a927
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-07-25 08:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 private: 
-    int houseRobber(vector<int>& nums, int start, int end) {
-        int one = nums[start];
-        int two = max(nums[start], nums[start+1]);
+    int helper(vector<int>& nums, int start, int end) {
+        int second = nums[start];
+        int first = max(nums[start], nums[start + 1]);
 
-        for(int i=start+2; i<=end; i++) {
-            int result = max(two, one + nums[i]);
-            one = two;
-            two = result;
+        for(int i = start + 2; i <= end; i++) {
+            int result = max(second + nums[i], first);
+            second = first;
+            first = result;
         }
 
-        return two;
+        return first;
     }
 
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        int no_zero = houseRobber(nums, 0, n-2);
-        int yes_zero = houseRobber(nums, 1, n-1);
+        int withZero = helper(nums, 0, n-2);
+        int withoutZero = helper(nums, 1, n-1);
 
-        return max(no_zero, yes_zero);
+        return max(withZero, withoutZero);
     }
 };
