8be11b6017e43c199652b7c44959340bb9c35af5
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-09-03 22:35:12 +0530

    Add Medium C++ Solution for 4258. Construct Uniform Parity Array II from LeetCode

---
+++ LeetCode/C++/Medium/4258. Construct Uniform Parity Array II/Solution.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n = nums1.size();
+        
+        bool odd = true;
+        bool even = true;
+
+        // Let us first do the odd case check
+        for(int i=0; i<n; i++) {
+            if(nums1[i] % 2 == 0) {
+                bool found = false;
+                for(int j=0; j<n; j++) {
+                    if(i != j && ((nums1[i] - nums1[j]) % 2 != 0) && (nums1[i] - nums1[j] >= 1)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) {
+                    odd = false;
+                    break;
+                }
+            }
+        }
+
+        // Now let us check for the even case
+        for(int i=0; i<n; i++) {
+            if(nums1[i] % 2 != 0) {
+                bool found = false;
+                for(int j=0; j<n; j++) {
+                    if(i != j && ((nums1[i] - nums1[j]) % 2 == 0) && (nums1[i] - nums1[j] >= 1)) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) {
+                    even = false;
+                    break;
+                }
+            }
+        }
+
+        return odd || even; 
+    }
+};
