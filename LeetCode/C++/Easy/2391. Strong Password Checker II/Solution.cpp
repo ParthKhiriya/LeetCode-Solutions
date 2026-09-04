a9367dd6ced944206fe63c9253e51a3c9046bcf8
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-09-04 15:32:32 +0530

    Add Easy C++ Solution for 2391. Strong Password Checker II from LeetCode

--- LeetCode/C++/Easy/2391. Strong Password Checker II/Solution.cpp
+++ LeetCode/C++/Easy/2391. Strong Password Checker II/Solution.cpp
@@ -8,26 +8,25 @@ public:
         bool uppercase = false;
         bool digit = false;
         bool special = false;
-        bool adjacentSame = true;
 
         for(int i=0; i<n; i++) {
             char c = password[i];
 
-            if(c > 'a' && c < 'z') {
+            if(i != 0 && password[i-1] == c) {
+                return false;
+            }
+
+            if(c >= 'a' && c <= 'z') {
                 lowercase = true;
-            } else if (c > 'A' && c < 'Z') {
+            } else if (c >= 'A' && c <= 'Z') {
                 uppercase = true;
-            } else if (c >= '0' && c < '9') {
+            } else if (c >= '0' && c <= '9') {
                 digit = true;
             } else if (c == '!' || c == '@' || c == '#' || c == '$' || c == '%' || c == '^' || c == '&' || c == '*' || c == '(' || c == ')' || c == '-' || c == '+') {
                 special = true;
             }
-
-            if(i > 0 && password[i-1] == c) {
-                adjacentSame = false;
-            }
         }
 
-        return (lowercase && uppercase && digit && special && adjacentSame);
+        return (lowercase && uppercase && digit && special);
     }
 };
