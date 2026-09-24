92f167ff865cc7124b6b2a3411b08d81884a27fc
Parth Khiriya <141862800+ParthKhiriya@users.noreply.github.com>
2026-09-25 01:00:51 +0530

    Add Medium C++ Solution for 542. 01 Matrix from LeetCode

--- LeetCode/C++/Medium/542. 01 Matrix/README.md
+++ LeetCode/C++/Medium/542. 01 Matrix/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming, Breadth-First Search, Matrix
 
 ### 🚀 Performance
-- **Runtime:** 22 ms
-- **Memory:** 38 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Medium/542. 01 Matrix/Solution.cpp
+++ LeetCode/C++/Medium/542. 01 Matrix/Solution.cpp
@@ -6,11 +6,11 @@ public:
         vector<vector<int>> ans(n, vector<int>(m, 0));
         vector<vector<bool>> vis(n, vector<bool>(m, false));
 
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<int, int>> q;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(mat[i][j] == 0) {
-                    q.push({{i, j}, 0});
+                    q.push({i, j});
                     vis[i][j] = true;
                 }
             }
@@ -20,9 +20,9 @@ public:
         int delCol[4] = {0, 1, 0, -1};
 
         while(!q.empty()) {
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int dist = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
+            int dist = ans[row][col];
             q.pop();
 
             for(int i=0; i<4; i++) {
@@ -30,7 +30,7 @@ public:
                 int newCol = col + delCol[i];
 
                 if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && vis[newRow][newCol] == false) {
-                    q.push({{newRow, newCol}, dist+1});
+                    q.push({newRow, newCol});
                     ans[newRow][newCol] = dist + 1;
                     vis[newRow][newCol] = true;
                 }
